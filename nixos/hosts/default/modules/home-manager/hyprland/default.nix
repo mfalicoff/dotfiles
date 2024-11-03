633db82b9cbc60bad3ebc98c4483e744a67d6515
Maximiliano Falicoff <mfalicoff@yahoo.com>
2024-11-03 12:10:01 -0500

    exratc hyprland to module

---
+++ nixos/hosts/default/modules/home-manager/hyprland/default.nix
@@ -0,0 +1,24 @@
+{config, lib, pkgs, ... }:
+
+{
+  options.modules.hyprland = {
+    enable = lib.mkEnableOption "hyprland configuration";
+  };
+
+  config = lib.mkIf config.modules.hyprland.enable {
+    wayland.windowManager.hyprland = {
+      enable = true;
+      extraConfig = builtins.readFile ./hyprland.conf;
+    };
+
+    services.hyprpaper = {
+      enable = true;
+      settings = {
+        wallpaper = [
+          ", /home/mazilious/.config/nixos/hosts/default/wallpapper.jpg"
+          ", /home/mazilious/.config/nixos/hosts/default/wallpapper.jpg"
+        ];
+      };
+    };
+  };
+}
