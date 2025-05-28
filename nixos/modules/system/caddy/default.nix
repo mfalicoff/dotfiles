fb5b19be97e20b7a508b0b880b36b399180fc61b
mfalicoff <mfalicoff@yahoo.com>
2025-05-28 09:42:17 -0400

    add more hosts

--- nixos/modules/system/caddy/default.nix
+++ nixos/modules/system/caddy/default.nix
@@ -1,13 +1,17 @@
-{config, lib, pkgs, ...}:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 with lib; let
   cfg = config.reverseProxy;
 in {
   options.reverseProxy = {
     enable = mkEnableOption "Enable Caddy";
   };
-  
-  config = mkIf cfg.enable {
 
+  config = mkIf cfg.enable {
     services.caddy = {
       enable = true;
       package = pkgs.caddy.withPlugins {
@@ -16,7 +20,7 @@ in {
       };
       logDir = "/var/log/caddy";
       dataDir = "/var/lib/caddy";
-      
+
       virtualHosts = {
         "beszel.caddy.mazilious.org" = {
           extraConfig = ''
@@ -26,9 +30,81 @@ in {
             }
           '';
         };
+
+        "books.caddy.mazilious.org" = {
+          extraConfig = ''
+            reverse_proxy http://100.94.140.88:8083
+            tls {
+              dns cloudflare {env.CF_API_TOKEN}
+            }
+          '';
+        };
+
+        "code.caddy.mazilious.org" = {
+          extraConfig = ''
+            reverse_proxy http://100.94.140.88:3200
+            tls {
+              dns cloudflare {env.CF_API_TOKEN}
+            }
+          '';
+        };
+
+        "daawarich.caddy.mazilious.org" = {
+          extraConfig = ''
+            reverse_proxy http://100.94.140.88:3008
+            tls {
+              dns cloudflare {env.CF_API_TOKEN}
+            }
+          '';
+        };
+
+        "glance.caddy.mazilious.org" = {
+          extraConfig = ''
+            reverse_proxy http://100.94.140.88:8280
+            tls {
+              dns cloudflare {env.CF_API_TOKEN}
+            }
+          '';
+        };
+
+        "immich.caddy.mazilious.org" = {
+          extraConfig = ''
+            reverse_proxy http://100.94.140.88:2283
+            tls {
+              dns cloudflare {env.CF_API_TOKEN}
+            }
+          '';
+        };
+
+        "jellyfin.caddy.mazilious.org" = {
+          extraConfig = ''
+            reverse_proxy http://100.94.140.88:8096
+            tls {
+              dns cloudflare {env.CF_API_TOKEN}
+            }
+          '';
+        };
+
+        "karapeep.caddy.mazilious.org" = {
+          extraConfig = ''
+            reverse_proxy http://100.94.140.88:3010
+            tls {
+              dns cloudflare {env.CF_API_TOKEN}
+            }
+          '';
+        };
+
+        "torrent.caddy.mazilious.org" = {
+          extraConfig = ''
+            reverse_proxy http://100.94.140.88:8096
+            tls {
+              dns cloudflare {env.CF_API_TOKEN}
+            }
+          '';
+        };
       };
     };
 
-    systemd.services.caddy.serviceConfig.EnvironmentFile = [ "/home/mazilious/dotfiles/nixos/modules/system/caddy/.env" ];
+    systemd.services.caddy.serviceConfig.EnvironmentFile = ["/home/mazilious/dotfiles/nixos/modules/system/caddy/.env"];
   };
 }
